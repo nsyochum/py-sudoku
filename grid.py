f3630e4642ec6112eabc729a186fe7641101d079
Vendor Test I <vendortest1@gitprime.com>
2019-11-14 22:33:28 -0800

    wrote recursive logic and added more methods to grid

--- grid.py
+++ grid.py
@@ -3,6 +3,8 @@ import csv
 
 
 class Grid:
+    """Class to handle puzzle logic"""
+
     # layout of the puzzle
     # row, column
     # -1 -1 -1 | -1 -1 -1 | -1 -1 -1
@@ -16,21 +18,26 @@ class Grid:
     # -1 -1 -1 | -1 -1 -1 | -1 -1 -1
     # -1 66 -1 | -1 77 -1 | -1 88 -1
     # -1 -1 -1 | -1 -1 -1 | -1 -1 -1
+    def __init__(self):
+        self.grid = np.zeros(9, 9)
 
-    grid = np.zeros(9, 9)
+    # sets the given location to the given number
+    def set_square(self, row, col, num):
+        self.grid[row, col] = num
+
+    # returns the value at a given square
+    def get_square(self, row, col):
+        return self.grid[row, col]
 
     # this method checks if the location is a valid position for the number in the grid
     # loc: a tuple in the form of (row, col)
     # num: a number 1-9 to check
     # returns True if this is valid, false otherwise
-    def is_valid(self, loc, num):
-        row = loc[0]
-        col = loc[1]
-
+    def is_valid(self, row, col, num):
         if (num, row, col) < (1, 1, 1) or (num, row, col) > (9, 9, 9):
             return False
 
-        return self.check_row(num, row) and self.check_column(num, col)
+        return self.check_row(num, row) and self.check_column(num, col) and self.check_square(row, col, num)
 
     # this method checks if the num is unique in the given row
     # num: the number to check
@@ -58,15 +65,55 @@ class Grid:
     # num: the number to check
     # loc: the location in the square
     # returns True if the number is valid in the square, False otherwise
-    def check_square(self, num, loc):
-        topRow = (loc[0] / 3) * 3
-        leftCol = (loc[1] / 3) * 3
+    def check_square(self, row, col, num):
+        topRow = (row / 3) * 3
+        leftCol = (col / 3) * 3
         for rowOffset in range(3):
             for colOffset in range(3):
                 if self.grid[topRow + rowOffset, leftCol + colOffset] == num:
                     return False
 
         return True
+
+    # checks if the puzzle is solved
+    def is_solved(self):
+        small_sum = sum(range(1, 10, 1))
+        puzzle_sum = small_sum * 9
+
+        # check total sum
+        total_sum = sum(self.grid)
+        if total_sum != puzzle_sum:
+            return False
+
+        # check each row
+        for row in range(9):
+            row_sum = sum(self.grid[row, :])
+            if row_sum != small_sum:
+                return False
+
+        # check each col
+        for col in range(9):
+            col_sum = sum(self.grid[:, col])
+            if col_sum != small_sum:
+                return False
+
+        # square sum
+        for col in range(3):
+            for row in range(3):
+                square_sum = self.sum_square(row*3, col*3)
+                if square_sum != small_sum:
+                    return False
+
+        return True
+
+    # sums over the square that starts on the given location
+    def sum_square(self, row, col):
+        square_sum = 0
+        for col_off in range(3):
+            for row_off in range(3):
+                square_sum += self.grid[row + row_off, col + col_off]
+
+        return square_sum
 
     # This method prints out the current grid to the console
     def print_grid(self):
@@ -100,8 +147,8 @@ class Grid:
 
     # This method parses a grid from the given file
     def parse_file(self, file_name):
-        with open(file_name) as csvfile:
-            reader = csv.reader(csvfile)
+        with open(file_name) as csv_file:
+            reader = csv.reader(csv_file)
             rownum = 0
             for row in reader:
                 self.grid[rownum,:] = [int(datum) for datum in row]
--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,24 @@
 import sys
+from grid import Grid
 
 file = str(sys.argv)
+puzzle = Grid()
+puzzle.parse_file(file)
 
-def parse_file(file):
+# 00 01 02 03 04 05 06 07 08 09
+# 10 11 12 13 14
+
+def solve(grid):
+    for row in range(9):
+        for col in range(9):
+            if grid.get_square(row, col) == 0:
+                for num in range(1,10,1):
+                    if grid.is_valid(row, col, num):
+                        grid.set_square(row, col, num)
+                        if solve(grid):
+                            return True
+                        else:
+                            grid.set_square(row, col, 0)
+
+                return False
+    return True
